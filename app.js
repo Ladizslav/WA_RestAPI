9da3bcda8766b33ee27d33b285524e8e1a188d35
Ladizslav <kidalsai@gmail.com>
2024-11-10 23:16:41 +0100

    Forthteenth commit

--- app.js
+++ app.js
@@ -129,7 +129,7 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: "Something went wrong!" });
 });
 
-app.listen(8080, () => {
+app.listen(80, () => {
     console.log("Server is running on port 8080");
 });
 
